10a4b24c9db4feaf5fe91349cc10ed3867d633cc
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-04-27 15:08:49 +0600

    added login and register handler

--- src/pages/shared/SignIn/SignIn.jsx
+++ src/pages/shared/SignIn/SignIn.jsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function SignIn() {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const handleLoginForm = (e) => {
+    e.preventDefault();
+    const user = { email, password };
+    console.log(user);
+  };
   return (
     <div>
       <div className="card shrink-0 w-full md:w-[550xp] mx-auto max-w-sm shadow-2xl bg-base-100">
-        <form className="card-body">
+        <form onSubmit={handleLoginForm} className="card-body">
           <h1 className="text-center text-3xl font-semibold">Login Form</h1>
           <div className="form-control">
             <label className="label">
@@ -16,6 +25,8 @@ function SignIn() {
               placeholder="email"
               className="input input-bordered"
               required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-control">
@@ -27,6 +38,8 @@ function SignIn() {
               placeholder="password"
               className="input input-bordered"
               required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <label className="label">
               <a href="#" className="label-text-alt link link-hover">
@@ -35,7 +48,9 @@ function SignIn() {
             </label>
           </div>
           <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
           </div>
           <p className="font-medium text-gray-600 text-center">
             I don't have account{" "}
--- src/pages/shared/SignUp/SignUp.jsx
+++ src/pages/shared/SignUp/SignUp.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function SignUp() {
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const handleRegisterForm = (e) => {
+    e.preventDefault();
+    const user = { name, email, photo, password };
+    console.log(user);
+  };
+
   return (
     <div>
       <div className="card shrink-0 w-full md:w-[550xp] mx-auto max-w-sm shadow-2xl bg-base-100">
-        <form className="card-body">
+        <form onSubmit={handleRegisterForm} className="card-body">
           <h1 className="text-center text-3xl font-semibold">
             Registration Form
           </h1>
@@ -17,6 +29,8 @@ function SignUp() {
               placeholder="Name"
               className="input input-bordered"
               required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="form-control">
@@ -28,6 +42,8 @@ function SignUp() {
               placeholder="email"
               className="input input-bordered"
               required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-control">
@@ -39,6 +55,8 @@ function SignUp() {
               placeholder="Photo URL"
               className="input input-bordered"
               required
+              value={photo}
+              onChange={(e) => setPhoto(e.target.value)}
             />
           </div>
           <div className="form-control">
@@ -50,12 +68,21 @@ function SignUp() {
               placeholder="password"
               className="input input-bordered"
               required
-            /> 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="form-control mt-6">
-            <button className="btn btn-primary">Register</button>
+            <button type="submit" className="btn btn-primary">
+              Register
+            </button>
           </div>
-          <p className="font-medium text-gray-600 text-center">I have an account <Link className="link text-blue-600" to="/login">Login</Link></p>
+          <p className="font-medium text-gray-600 text-center">
+            I have an account{" "}
+            <Link className="link text-blue-600" to="/login">
+              Login
+            </Link>
+          </p>
         </form>
       </div>
     </div>
