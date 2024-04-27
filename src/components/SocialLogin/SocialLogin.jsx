b02ccb1c79576b5159291f75301a35169970a623
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-04-27 23:18:23 +0600

    added social media login system

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,6 +1,21 @@
+import { useContext } from "react";
 import { FaGoogle, FaGithub, FaFacebook } from "react-icons/fa";
+import { AuthContext } from "../../contextApi/AuthProvider";
 
 function SocialLogin() {
+  const { googleSignIn, githubSignIn, facebookSignIn } =
+    useContext(AuthContext);
+
+  const handleSignIn = (signIn) => {
+    signIn()
+      .then((res) => {
+        console.log(res.user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <fieldset className="border rounded-xl mt-4 mb-2">
       <legend className="text-center font-semibold text-primary">
@@ -8,19 +23,19 @@ function SocialLogin() {
       </legend>
       <div className="flex gap-4 justify-around items-center py-4">
         <div
-        //   onClick={handleGoogleSignIn}
+          onClick={() => handleSignIn(googleSignIn)}
           className="btn rounded-full bg-red-800 hover:bg-red-900 text-white"
         >
           <FaGoogle />
         </div>
         <div
-        //   onClick={handleGithubSignIn}
+          onClick={() => handleSignIn(githubSignIn)}
           className="btn rounded-full bg-gray-700 hover:bg-gray-800 text-white"
         >
           <FaGithub />
         </div>
         <div
-        //   onClick={handleFacebookSignIn}
+          onClick={() => handleSignIn(facebookSignIn)}
           className="btn rounded-full bg-blue-500 text-white hover:bg-blue-600"
         >
           <FaFacebook />
